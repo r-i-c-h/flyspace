9c43d27d42ba9dffeb003f75ce2414d317c44f16
r-i-c-h <rich@richwerden.com>
2023-05-11 14:21:41 -0400

    🎨🧹remove cruft, small refactor <main>/.main

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ import { roboto } from './fonts'
 
 export default function Home() {
   return (
-    <main className={`${styles.main} ${roboto.className}`}>
+    <main className={`${roboto.className}`}>
       <h1>What the?</h1>
+      <h2>The mostest FLYist space place to be social</h2>
     </main>
   )
 }
