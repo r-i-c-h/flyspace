4a8d3117e9b20feda790c207bbcfd8823698fb74
r-i-c-h <rich@richwerden.com>
2023-05-11 14:14:04 -0400

    🎨: Custom styles/reword for static 'about' page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import styles from './page.module.css';
 
 export const dynamic = 'force-static'; // <~~ Not actually necessary
 
@@ -10,11 +11,11 @@ export const metadata: Metadata = {
 
 export default async function About() {
 
-  return <main>
+  return (<main>
     <h1>So About that <em>About</em> now....</h1>
-    <p>Here is some static content that is unchanging.</p>
+    <p>Here is some <span className={styles.custom}>static</span> content that is unchanging.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sed non officiis libero reprehenderit numquam ratione, fugiat quis laborum architecto harum officia placeat iste autem ipsam voluptate nemo pariatur quibusdam.
       Exercitationem, voluptas accusantium? Cum dolorum sint minima maiores dicta a consequatur error, odio animi laboriosam quia sapiente ipsam, distinctio, quasi rem numquam sequi eaque explicabo voluptates corrupti ratione. Neque, eligendi.</p>
-  </main>
+  </main>)
 
 }
