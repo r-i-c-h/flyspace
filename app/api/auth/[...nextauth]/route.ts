0369ca4b78d1c23465f6533c12ba9a0e1c5b26e0
r-i-c-h <rich@richwerden.com>
2023-05-23 18:08:31 -0400

    start add PrismaDB,+dotenv,+package scripts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,13 @@
+import { prisma } from '@/lib/prisma';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { CredentialsProvider } from 'next-auth/providers';
 
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
