4aec7aa5929f4b7a8930dcd52d5db41dabbf65d3
r-i-c-h <rich@richwerden.com>
2023-05-23 22:19:38 -0400

    +getAllUsers() api route/query via Prisma

---
+++ app/api/users/route.tsx
@@ -0,0 +1,10 @@
+import { prisma } from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+/**** Query all Users and return them as a JSON object ****/
+export async function GET(request: Request) {
+  const users = await prisma.user.findMany();
+  console.log(users);
+
+  return NextResponse.json(users);
+}
